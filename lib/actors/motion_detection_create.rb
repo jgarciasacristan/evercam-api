c0db406544aec29d155f457885e9af972102365f
Eugene HP <eugene.hp2012@gmail.com>
2015-08-20 19:25:55 +0200

    update on motion_detection table creation

--- lib/actors/motion_detection_create.rb
+++ lib/actors/motion_detection_create.rb
@@ -15,16 +15,6 @@ module Evercam
       def execute
         camera = Camera.by_exid!(inputs[:camera_id])
 
-        if inputs["region_of_interest"].blank?
-          region_of_interest = {}
-        else
-          begin
-            region_of_interest = JSON.parse(inputs["region_of_interest"])
-          rescue => _e
-            add_error(:region_of_interest, :invalid, "The parameter 'region_of_interest' isn't formatted as a proper JSON.")
-          end
-        end
-
         motion_detection = MotionDetection.create(
           camera: camera,
           region_of_interest: region_of_interest
@@ -32,6 +22,16 @@ module Evercam
           datetime: inputs["datetime"],
           recent_images: inputs["recent_images"]
         )
+
+        unless inputs["region_of_interest"].blank?
+          begin
+            motion_detection.region_of_interest = JSON.parse(inputs["region_of_interest"])
+          rescue => _e
+            add_error(:region_of_interest, :invalid, "The parameter 'region_of_interest' isn't formatted as a proper JSON.")
+          end
+        end
+
+        motion_detection.save
         motion_detection
       end
     end
